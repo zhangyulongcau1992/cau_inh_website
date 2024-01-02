f46195348e03e184a5e0c461f9d322f5e10155ec
zhangyulongcau1992 <zhangyulong_cau@163.com>
2024-01-02 21:51:23 +0800

    Initial commit

---
+++ management_server/src/main/java/cn/edu/cau/inh/management_server/ManagementServerApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.cau.inh.management_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ManagementServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ManagementServerApplication.class, args);
+    }
+
+}
